081da08d2481887da5c1b81f28bb04634b8d143e
Trond A Ekseth <troeks@gmail.com>
2014-10-18 20:25:44 +0200

    Make file uploading more ajaxy.

--- page/upload.go
+++ page/upload.go
@@ -1,14 +1,11 @@
 package page
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 	"regexp"
 
-	"tiers/conf"
 	"tiers/queue"
 	"tiers/session"
 	"time"
@@ -54,12 +51,12 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// XXX: Handle errors.
-	var db, _ = sql.Open("mysql", conf.Config.Database)
-	defer db.Close()
+	// var db, _ = sql.Open("mysql", conf.Config.Database)
+	// defer db.Close()
 
 	for {
 		part, err := reader.NextPart()
-		if err == io.EOF {
+		if err != nil {
 			break
 		}
 
@@ -82,30 +79,46 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			fileName = fmt.Sprintf("%d_%s.png", userid, t.Format("20060102_150405"))
 		}
 
-		dst, err := os.Create(conf.Config.Cache + fileName)
-		defer dst.Close()
+		fmt.Println(fileName)
 
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		/*
+			dst, err := os.Create(conf.Config.Cache + fileName)
+			defer dst.Close()
 
-		if _, err := io.Copy(dst, part); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if _, err := io.Copy(dst, part); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		*/
 
 		// XXX: Handle errors...
-		db.Exec(`
-		INSERT INTO tiers_queues(user_id, timestamp, file)
-		VALUES(?, ?, ?)
-		`, userid, t.Unix(), fileName)
+		//db.Exec(`
+		//INSERT INTO tiers_queues(user_id, timestamp, file)
+		//VALUES(?, ?, ?)
+		//`, userid, t.Unix(), fileName)
 	}
 
 	var numQueue float32
-	db.QueryRow(`SELECT count(id) FROM tiers_queues WHERE processed = 0`).Scan(&numQueue)
+	//db.QueryRow(`SELECT count(id) FROM tiers_queues WHERE processed = 0`).Scan(&numQueue)
+	queueText := fmt.Sprintf(
+		"Your file has been added to the queue and should be processed within %.1f seconds.",
+		numQueue*6.6,
+	)
 
-	fmt.Fprintf(w, "Your file has been added to the queue and should be processed in %.1f seconds.", numQueue*6.6)
+	if r.Header.Get("X-Requested-With") == "XMLHttpRequest" {
+		data, _ := json.Marshal(map[string]interface{}{
+			"success": true,
+			"message": queueText,
+		})
+		fmt.Fprintf(w, "%s", data)
+	} else {
+		http.Redirect(w, r, "/", 302)
+	}
 
 	queue.Queue <- true
 }
--- static/js/app.js
+++ static/js/app.js
@@ -25,3 +25,50 @@ $(".btn-forgot").click(function(event) {
 	$(".signup-box").hide();
 	$(".forgot-box").show();
 });
+
+// Upload
+$(".form-upload").submit(function(event) {
+	event.preventDefault();
+
+	$.ajax({
+		url: "/upload",
+		type: "POST",
+		data: new FormData(this),
+		dataType: "json",
+		processData: false,
+		contentType: false,
+
+		beforeSend: function() {
+			$(".form-upload").find("button").prop("disabled", true);
+			$(".progress").hide().removeClass("hide").fadeIn();
+		},
+
+		success: function(data, textStatus, xhr) {
+			$(".alert")
+			.hide()
+			.removeClass("hide")
+			.text(data.message)
+			.fadeIn();
+		},
+
+		xhr: function() {
+			var xhr = new window.XMLHttpRequest();
+
+			xhr.upload.addEventListener("progress", function(event) {
+				if (event.lengthComputable) {
+					var percentage = Math.floor(event.loaded / event.total * 100);
+					$(".progress-bar").width(percentage + "%");
+
+					if(percentage === 100) {
+						$(".progress").delay(250).fadeOut(function() {
+							$(".form-upload").find("button").prop("disabled", false);
+							$(".progress-bar").width(0);
+						});
+					}
+				}
+			}, false);
+
+			return xhr;
+		}
+	});
+});
