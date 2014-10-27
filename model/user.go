1edb2d6cf219749a786b0e48c9797662d124af06
Trond A Ekseth <troeks@gmail.com>
2014-10-27 22:40:51 +0100

    Initial diff display.

--- model/user.go
+++ model/user.go
@@ -272,3 +272,45 @@ func GetNewestProfile(user_id int) profile.Profile {
 
 	return p
 }
+
+func GetNewestProfiles(user_id, limit int) []profile.Profile {
+	var profiles []profile.Profile
+
+	// XXX: Handle errors.
+	row, _ := db.Query(`
+		SELECT id, user_id, timestamp, agent, level, ap,
+		unique_portals_visited, portals_discovered, xm_collected,
+		hacks, resonators_deployed, links_created, control_fields_created, mind_units_captured,
+		longest_link_ever_created, largest_control_field, xm_recharged, portals_captured,
+		unique_portals_captured,
+		resonators_destroyed, portals_neutralized, enemy_links_destroyed, enemy_control_fields_destroyed,
+		distance_walked,
+		max_time_portal_held, max_time_link_maintained, max_link_length_x_days, max_time_field_held,
+		largest_field_mus_x_days
+		FROM tiers_profiles
+		WHERE user_id = ?
+		ORDER BY timestamp DESC
+		LIMIT ?
+		`, user_id, limit)
+	defer row.Close()
+
+	for row.Next() {
+		var p profile.Profile
+		row.Scan(
+			&p.Id, &p.UserId, &p.Timestamp,
+			&p.Nick, &p.Level, &p.AP,
+			&p.UniquePortalsVisited, &p.PortalsDiscovered, &p.XMCollected,
+			&p.Hacks, &p.ResonatorsDeployed, &p.LinksCreated, &p.ControlFieldsCreated, &p.MindUnitsCaptured,
+			&p.LongestLinkEverCreated, &p.LargestControlField, &p.XMRecharged, &p.PortalsCaptured,
+			&p.UniquePortalsCaptured,
+			&p.ResonatorsDestroyed, &p.PortalsNeutralized, &p.EnemyLinksDestroyed, &p.EnemyControlFieldsDestroyed,
+			&p.DistanceWalked,
+			&p.MaxTimePortalHeld, &p.MaxTimeLinkMaintained, &p.MaxLinkLengthXDays, &p.MaxTimeFieldHeld,
+			&p.LargestFieldMUsXDays,
+		)
+
+		profiles = append(profiles, p)
+	}
+
+	return profiles
+}
--- page/profile.go
+++ page/profile.go
@@ -4,12 +4,62 @@ import (
 	"net/http"
 
 	"tiers/model"
+	"tiers/profile"
 	"tiers/session"
 )
 
+type ProfilePageData struct {
+	Profile profile.Profile
+	Diff    interface{}
+}
+
 type ProfilePage struct {
 	User int
-	Data interface{}
+	Data ProfilePageData
+}
+
+func createDiff(a, b profile.Profile) profile.Profile {
+	var diff profile.Profile
+
+	diff.Level = b.Level - a.Level
+	diff.AP = b.AP - a.AP
+
+	diff.Bronze = b.Bronze - a.Bronze
+	diff.Silver = b.Silver - a.Silver
+	diff.Gold = b.Gold - a.Gold
+	diff.Platinum = b.Platinum - a.Platinum
+	diff.Onyx = b.Onyx - a.Onyx
+
+	diff.UniquePortalsVisited = b.UniquePortalsVisited - a.UniquePortalsVisited
+	diff.PortalsDiscovered = b.PortalsDiscovered - a.PortalsDiscovered
+	diff.XMCollected = b.XMCollected - a.XMCollected
+
+	diff.Hacks = b.Hacks - a.Hacks
+	diff.ResonatorsDeployed = b.ResonatorsDeployed - a.ResonatorsDeployed
+	diff.LinksCreated = b.LinksCreated - a.LinksCreated
+	diff.ControlFieldsCreated = b.ControlFieldsCreated - a.ControlFieldsCreated
+	diff.MindUnitsCaptured = b.MindUnitsCaptured - a.MindUnitsCaptured
+	diff.LongestLinkEverCreated = b.LongestLinkEverCreated - a.LongestLinkEverCreated
+	diff.LargestControlField = b.LargestControlField - a.LargestControlField
+	diff.XMRecharged = b.XMRecharged - a.XMRecharged
+	diff.PortalsCaptured = b.PortalsCaptured - a.PortalsCaptured
+	diff.UniquePortalsCaptured = b.UniquePortalsCaptured - a.UniquePortalsCaptured
+
+	diff.ResonatorsDestroyed = b.ResonatorsDestroyed - a.ResonatorsDestroyed
+	diff.PortalsNeutralized = b.PortalsNeutralized - a.PortalsNeutralized
+	diff.EnemyLinksDestroyed = b.EnemyLinksDestroyed - a.EnemyLinksDestroyed
+	diff.EnemyControlFieldsDestroyed = b.EnemyControlFieldsDestroyed - a.EnemyControlFieldsDestroyed
+
+	diff.DistanceWalked = b.DistanceWalked - a.DistanceWalked
+
+	diff.MaxTimePortalHeld = b.MaxTimePortalHeld - a.MaxTimePortalHeld
+	diff.MaxTimeLinkMaintained = b.MaxTimeLinkMaintained - a.MaxTimeLinkMaintained
+	diff.MaxLinkLengthXDays = b.MaxLinkLengthXDays - a.MaxLinkLengthXDays
+	diff.MaxTimeFieldHeld = b.MaxTimeFieldHeld - a.MaxTimeFieldHeld
+
+	diff.LargestFieldMUsXDays = b.LargestFieldMUsXDays - a.LargestFieldMUsXDays
+
+	return diff
 }
 
 func ProfileHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,11 +75,25 @@ func ProfileHandler(w http.ResponseWriter, r *http.Request) {
 	)
 
 	if ok {
-		p := model.GetNewestProfile(userid.(int))
+		var p profile.Profile
+		var diff interface{}
+
+		profiles := model.GetNewestProfiles(userid.(int), 2)
+
+		switch len(profiles) {
+		case 1:
+			p = profiles[0]
+		case 2:
+			diff = createDiff(profiles[1], profiles[0])
+			p = profiles[0]
+		}
 
 		templates.ExecuteTemplate(w, "profile", ProfilePage{
 			User: userid.(int),
-			Data: p,
+			Data: ProfilePageData{
+				Profile: p,
+				Diff:    diff,
+			},
 		})
 	} else {
 		templates.ExecuteTemplate(w, "index-unauthed", nil)
