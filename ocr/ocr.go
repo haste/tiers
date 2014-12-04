2a99b7d303d3fd7afeb3f0701399a144f8625285
Trond A Ekseth <troeks@gmail.com>
2014-12-04 23:03:18 +0100

    Automate generation of OCR patterns.

--- ocr/ocr.go
+++ ocr/ocr.go
@@ -21,13 +21,9 @@ type innovator struct {
 }
 
 func sanitizeNum(input []byte) int64 {
+	input = regexp.MustCompile(`[lL|\]JI]`).ReplaceAll(input, []byte("1"))
+
 	n := string(input)
-	n = strings.Replace(n, "l", "1", -1)
-	n = strings.Replace(n, "L", "1", -1)
-	n = strings.Replace(n, "|", "1", -1)
-	n = strings.Replace(n, "]", "1", -1)
-	n = strings.Replace(n, "J", "1", -1)
-	n = strings.Replace(n, "I", "1", -1)
 	n = strings.Replace(n, "o", "0", -1)
 	n = strings.Replace(n, "B", "8", -1)
 	n = strings.Replace(n, ",", "", -1)
@@ -56,43 +52,55 @@ func matchNum(res []byte, pattern string) int64 {
 	return sanitizeNum(r.FindSubmatch(res)[1])
 }
 
+func genMatchNum(res []byte, s string) int64 {
+	s = regexp.MustCompile(`[Ss]`).ReplaceAllLiteralString(s, "[Ss]")
+	s = regexp.MustCompile(`[ae]`).ReplaceAllLiteralString(s, "[ae]")
+	s = regexp.MustCompile(`[Pp]`).ReplaceAllLiteralString(s, "[Pp]")
+	s = regexp.MustCompile(`[Oo]`).ReplaceAllLiteralString(s, "[0Oo]")
+	s = regexp.MustCompile(`\s+`).ReplaceAllLiteralString(s, `\s*`)
+
+	s = strings.Replace(s, `-`, ".", -1)
+	s = strings.Replace(s, `#`, `([0-9LIl|J,B\]]+)`, -1)
+
+	return matchNum(res, s)
+}
+
 func buildProfile(res []byte) profile.Profile {
-	var digit = `([0-9LIl|J,B\]]+)`
 	var p profile.Profile
 
 	p.Nick = matchString(res, "([a-zA-Z0-9]+)[^\n]*\\s*[^\n]*LVL")
-	p.Level = int(matchNum(res, "LVL\\s*"+digit))
-	p.AP = matchNum(res, digit+"\\s*A[Pp]")
+	p.Level = int(genMatchNum(res, "LVL #"))
+	p.AP = genMatchNum(res, "# AP")
 
-	p.UniquePortalsVisited = matchNum(res, "Uniqu[ae]\\s*Port[ae]ls\\s*Visit[ae]d\\s*"+digit)
-	p.PortalsDiscovered = matchNum(res, "Port[ae]ls\\s*Discov[ae]r[ae]d\\s*"+digit)
-	p.XMCollected = matchNum(res, "XM\\s*Coll[ae]ct[ae]d\\s*"+digit+"\\s*XM")
+	p.UniquePortalsVisited = genMatchNum(res, "Unique Portals Visited #")
+	p.PortalsDiscovered = genMatchNum(res, "Portals Discovered #")
+	p.XMCollected = genMatchNum(res, "XM Collected # XM")
 
-	p.Hacks = matchNum(res, "H[ae]cks\\s*"+digit)
-	p.ResonatorsDeployed = matchNum(res, "R[ae]son[ae]tors\\s*D[ae]ploy[ae]d\\s*"+digit)
-	p.LinksCreated = matchNum(res, "Links\\s*Cr[ae][ae]t[ae]d\\s*"+digit)
-	p.ControlFieldsCreated = matchNum(res, "Control\\s*Fi[ae]lds\\s*Cr[ae][ae]t[ae]d\\s*"+digit)
-	p.MindUnitsCaptured = matchNum(res, "Mind\\s*Units\\s*C[ae]ptur[ae]d\\s*"+digit)
-	p.LongestLinkEverCreated = matchNum(res, "Long[ae]st\\s*Link\\s*Ev[ae]r\\s*Cr[ae][ae]t[ae]d\\s*"+digit+"\\s*km")
-	p.LargestControlField = matchNum(res, "L[ae]rg[ae]st\\s*Control\\s*Fi[ae]ld\\s*"+digit+"\\s*MUs")
-	p.XMRecharged = matchNum(res, "XM\\s*R[ae]ch[ae]rg[ae]d "+digit+"\\s*XM")
-	p.PortalsCaptured = matchNum(res, "Port[ae]ls\\s*C[ae]ptur[ae]d\\s*"+digit)
-	p.UniquePortalsCaptured = matchNum(res, "Uniqu[ae]\\s*Port[ae]ls\\s*C[ae]ptur[ae]d\\s*"+digit)
+	p.Hacks = genMatchNum(res, "Hacks #")
+	p.ResonatorsDeployed = genMatchNum(res, "Resonators Deployed #")
+	p.LinksCreated = genMatchNum(res, "Links Created #")
+	p.ControlFieldsCreated = genMatchNum(res, "Control Fields Created #")
+	p.MindUnitsCaptured = genMatchNum(res, "Mind Units Captured # MUs")
+	p.LongestLinkEverCreated = genMatchNum(res, "Longest Link Ever Created # km")
+	p.LargestControlField = genMatchNum(res, "Largest Control Field # MUs")
+	p.XMRecharged = genMatchNum(res, "XM Recharged # XM")
+	p.PortalsCaptured = genMatchNum(res, "Portals Captured #")
+	p.UniquePortalsCaptured = genMatchNum(res, "Unique Portals Captured #")
 
-	p.ResonatorsDestroyed = matchNum(res, "R[ae]son[ae]tors\\s*D[ae]stroy[ae]d\\s*"+digit)
-	p.PortalsNeutralized = matchNum(res, "Port[ae]ls\\s*N[ae]utr[ae]liz[ae]d\\s*"+digit)
-	p.EnemyLinksDestroyed = matchNum(res, "En[ae]my\\s*Links\\s*D[ae]stroy[ae]d\\s*"+digit)
-	p.EnemyControlFieldsDestroyed = matchNum(res, "En[ae]my\\s*Control\\s*Fi[ae]lds\\s*D[ae]stroy[ae]d\\s*"+digit)
+	p.ResonatorsDestroyed = genMatchNum(res, "Resonators Destroyed #")
+	p.PortalsNeutralized = genMatchNum(res, "Portals Neutralized #")
+	p.EnemyLinksDestroyed = genMatchNum(res, "Enemy Links Destroyed #")
+	p.EnemyControlFieldsDestroyed = genMatchNum(res, "Enemy Control Fields Destroyed #")
 
-	p.DistanceWalked = matchNum(res, "Dist[ae]nc[ae]\\s*W[ae]lk[ae]d\\s*"+digit)
+	p.DistanceWalked = genMatchNum(res, "Distance Walked # km")
 
-	p.MaxTimePortalHeld = matchNum(res, "M[ae]x\\s*Tim[ae]\\s*Port[ae]l\\s*H[ae]ld\\s*"+digit+"\\s*d[ae]ys")
-	p.MaxTimeLinkMaintained = matchNum(res, "M[ae]x\\s*Tim[ae]\\s*Link\\s*M[ae]int[ae]in[ae]d\\s*"+digit+"\\s*d[ae]ys")
-	p.MaxLinkLengthXDays = matchNum(res, "M[ae]x\\s*Link\\s*L[ae]ngth\\s*x\\s*D[ae]ys\\s*"+digit+"\\s*km.d[ae]ys")
-	p.MaxTimeFieldHeld = matchNum(res, "M[ae]x\\s*Tim[ae]\\s*Fi[ae]ld\\s*H[ae]ld\\s*"+digit+"\\s*d[ae]ys")
-	p.LargestFieldMUsXDays = matchNum(res, "L[ae]rg[ae]st\\s*Fi[ae]ld\\s*MUs\\s*x\\s*D[ae]ys\\s*"+digit+"\\s*MU.d[ae]ys")
+	p.MaxTimePortalHeld = genMatchNum(res, "Max Time Portal Held # days")
+	p.MaxTimeLinkMaintained = genMatchNum(res, "Max Time Link Maintained # days")
+	p.MaxLinkLengthXDays = genMatchNum(res, "Max Link Length x Days # km-days")
+	p.MaxTimeFieldHeld = genMatchNum(res, "Max Time Field Held # days")
+	p.LargestFieldMUsXDays = genMatchNum(res, "Largest Field MUs x Days # MU-days")
 
-	p.UniqueMissionsCompleted = matchNum(res, "Uniqu[ae]\\s*Missions\\s*Compl[ae]t[ae]d\\s*"+digit)
+	p.UniqueMissionsCompleted = genMatchNum(res, "Unique Missions Completed #")
 
 	return p
 }
@@ -107,7 +115,8 @@ func runOCR(fileName string) profile.Profile {
 	cvFile := conf.Config.Cache + "cv_" + fileName
 	tmpFile := conf.Config.Cache + "tmp_" + fileName
 
-	cv := exec.Command(conf.Config.UtilsDir+"innovator-crop/crop.py", []string{
+	cv := exec.Command(conf.Config.PythonBin, []string{
+		conf.Config.UtilsDir + "innovator-crop/crop.py",
 		conf.Config.Cache,
 		fileName,
 	}...)
