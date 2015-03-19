44d42af5c7ac009bf28eb2ad45e508ea6b755791
Trond A Ekseth <troeks@gmail.com>
2015-03-19 20:08:50 +0100

    Cover the menu.

--- utils/innovator-crop/crop.py
+++ utils/innovator-crop/crop.py
@@ -280,6 +280,9 @@ top = top[0:offset_bottom, :view.shape[1]]
 # cover up the share button
 cv2.rectangle(top, (top.shape[1] // 8 * 6, 0), (top.shape[1], top.shape[0] // 10 * 6), (0, 0, 0), cv.CV_FILLED)
 
+# cover menu.
+cv2.rectangle(top, (0, 0), (top.shape[1], top.shape[0] // 3), (0, 0, 0), cv.CV_FILLED)
+
 # Lines below
 bottom = view[achieve_bottom:, :view.shape[1]]
 
